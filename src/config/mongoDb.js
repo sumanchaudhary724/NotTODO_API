98f96a01759f0878796c8b39af38b6e335fccd55
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-06-24 10:37:45 +1000

    build

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 export const mongoConnect = async () => {
   try {
-    const con = await mongoose.connect("mongodb://0.0.0.0:27017/nottododb");
-
+    const dbLink =
+      "mongodb+srv://sumanchaudhary724:ccscuEnrRknVdyxF@cluster0.au3r6oj.mongodb.net/?retryWrites=true&w=majority";
+    // const con = await mongoose.connect("mongodb://0.0.0.0:27017/nottododb");
+    const con = await mongoose.connect(dbLink);
     con && console.log("mongo is connected");
   } catch (error) {
     console.log(error);
